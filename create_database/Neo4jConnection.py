f70e6683f9f927b38968ccd729502d78fb0205e1
ReeveWu <reeve0319@gmail.com>
2024-03-03 16:08:38 +0800

    add: insert into graph db

---
+++ create_database/Neo4jConnection.py
@@ -0,0 +1,55 @@
+from neo4j import GraphDatabase
+
+class Neo4jConnection:
+    
+    def __init__(self, uri, user, pwd):
+        self.__uri = uri
+        self.__user = user
+        self.__pwd = pwd
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
+        except Exception as e:
+            print("Failed to create the driver:", e)
+        
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+        
+    def create_movie_graph(self, movie_title, overview, genres, acting, directing):
+        with self.__driver.session() as session:
+            result = session.write_transaction(self._create_and_return_movie, movie_title, overview, genres, acting, directing)
+            print("Created movie graph for:", result)
+    
+    @staticmethod
+    def _create_and_return_movie(tx, movie_title, overview, genres, acting, directing):
+        overview = overview if overview else "No overview provided"
+        genres = genres if genres else []
+        acting = acting if acting else []
+        directing = directing if directing else []
+        
+        query = (
+            "MERGE (m:Movie {title: $movie_title, overview: $overview}) "
+            "FOREACH (genre in $genres | "
+            "   MERGE (g:Genre {name: genre}) "
+            "   MERGE (m)-[:HAS_GENRE]->(g)) "
+            "FOREACH (actor in $acting | "
+            "   MERGE (a:Actor {name: actor}) "
+            "   MERGE (m)-[:HAS_ACTOR]->(a)) "
+            "FOREACH (director in $directing | "
+            "   MERGE (d:Director {name: director}) "
+            "   MERGE (m)-[:HAS_DIRECTOR]->(d)) "
+            "RETURN m.title AS title"
+        )
+        result = tx.run(query, movie_title=movie_title, overview=overview, genres=genres, acting=acting, directing=directing)
+        return result.single()[0]
+    
+    def delete_all(self):
+        with self.__driver.session() as session:
+            session.write_transaction(self._delete_all_nodes_and_relations)
+            print("Deleted all nodes and relations.")
+
+    @staticmethod
+    def _delete_all_nodes_and_relations(tx):
+        query = "MATCH (n) DETACH DELETE n"
+        tx.run(query)
---
+++ create_database/db_execute.py
@@ -0,0 +1,47 @@
+import os
+import time
+import pandas as pd
+from dotenv import load_dotenv
+from Neo4jConnection import Neo4jConnection
+
+load_dotenv()
+
+uri = os.getenv('NEO4J_CONNECTION_URL')
+user = os.getenv('NEO4J_USER')
+password = os.getenv('NEO4J_PASSWORD')
+print(uri)
+
+def insert(conn: Neo4jConnection, movie_title: str, overview: str, genres: list, acting: list, directing: list):
+    overview = "" if pd.isna(overview) else overview
+    genres = [] if pd.isna(genres) else eval(genres)
+    acting = [] if pd.isna(acting) else eval(acting)
+    directing = [] if pd.isna(directing) else eval(directing)
+
+    conn.create_movie_graph(movie_title, overview, genres, acting, directing)
+
+def truncate(conn):
+    conn.delete_all()
+
+if __name__ == "__main__":
+    conn = Neo4jConnection(uri, user, password)
+
+    truncate(conn)
+
+    df = pd.read_csv('../data/movie_data.csv')
+    data = df.to_dict(orient='records')
+    for d in data:
+        try:
+            insert(conn=conn, 
+                movie_title=d['title'], 
+                overview=d['overview'], 
+                genres=d['genres'], 
+                acting=d['acting'], 
+                directing=d['directing']
+                )
+        except Exception as e:
+            print(f"Error: {e}")
+            
+    conn.close()
+
+    while True:
+        time.sleep(1)
